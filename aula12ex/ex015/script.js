3b97b369869d139e5d81a018da7480ac91361dbf
JrTheDev <junior.za@gmail.com>
2023-08-08 22:38:06 -0300

    projeto de idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,16 +6,46 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        let fsex = document.getElementsByTagName('radsex')
+        let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'homem-crianca.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src','homem-adolescente.png' )
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'homem-idoso.png')
+            }
+               
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'mulher-crianca.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'mulher-adolescente.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher-adulto.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'mulher-idoso.png')
+            }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `Detectamos <strong>${genero}</strong> com ${idade} anos.`
+        res.appendChild(img)
     }
 }
      
